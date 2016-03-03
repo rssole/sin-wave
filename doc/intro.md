cbd02649fc166cbb5c03412e8a939c06cfe3d50a
Rastko Soskic <rastko.soskic@gtech.com>
2016-03-03 16:10:05 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sin-wave
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/sin_wave/core_test.clj
@@ -0,0 +1,7 @@
+(ns sin-wave.core-test
+  (:require [clojure.test :refer :all]
+            [sin-wave.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
