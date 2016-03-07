61929d451bb0bcac0324f03b5fd45da7903b9c56
rastko <rssole@gmail.com>
2016-03-07 07:53:04 +0100

    Canvas for react prog sin-wave is here

--- src/cljs/sin_wave/core.cljs
+++ src/cljs/sin_wave/core.cljs
@@ -4,5 +4,33 @@
 ;; enable cljs to print to the JS console of the browser
 (enable-console-print!)
 
-;; print to the console
-(println "Hello, World Reloaded!")
+(def canvas (.getElementById js/document "myCanvas"))
+(def ctx    (.getContext canvas "2d"))
+
+;; Clear canvas before doing anything else
+(.clearRect ctx 0 0 (.-width canvas) (.-height canvas))
+
+(def interval   js/Rx.Observable.interval)
+(def sw-time       (interval 10))
+
+(defn deg-to-rad [n]
+  (* (/ Math/PI 180) n))
+
+(defn sine-coord [x]
+  (let [sin (Math/sin (deg-to-rad x))
+        y   (- 100 (* sin 90))]
+    {:x   x
+     :y   y
+     :sin sin}))
+
+(def sine-wave
+  (.map sw-time sine-coord))
+
+(defn fill-rect [x y colour]
+  (set! (.-fillStyle ctx) colour)
+  (.fillRect ctx x y 2 2))
+
+(-> sine-wave
+      (.take 600)
+      (.subscribe (fn [{:keys [x y]}]
+                    (fill-rect x y "orange"))))
