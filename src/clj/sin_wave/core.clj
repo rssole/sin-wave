4e2221b45a1efb6a15fb119d07ffe797a18793f1
Rastko Soskic <rastko.soskic@gtech.com>
2016-03-23 17:17:47 +0100

    Pre tutorial 12

--- src/clj/sin_wave/core.clj
+++ src/clj/sin_wave/core.clj
@@ -1,9 +1,11 @@
 (ns sin-wave.core
-  (:require [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [not-found files resources]]))
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [compojure.route :refer [not-found files resources]]
+            [sin-wave.login :refer [authenticate-user]]))
 
 (defroutes handler
-           (GET "/" [] "Hello from Compojure!")             ;; for testing only
-           (files "/" {:root "target"})                     ;; to serve static resources
-           (resources "/" {:root "target"})                 ;; to serve anything else
-           (not-found "Page Not Found"))                    ;; page not found
+           (GET "/" [] "Hello from Compojure!")  ;; for testing only
+           (files "/" {:root "target"})          ;; to serve static resources
+           (POST "/login" [email password] (authenticate-user email password))
+           (resources "/" {:root "target"})      ;; to serve anything else
+           (not-found "Page Not Found"))         ;; page not found
--- src/cljs/sin_wave/login.cljs
+++ src/cljs/sin_wave/login.cljs
@@ -4,31 +4,25 @@
                                  by-id
                                  destroy!
                                  prepend!
-                                 value]]
+                                 value
+                                 attr]]
             [domina.events :refer [listen! prevent-default]]
             [hiccups.runtime])
   (:require-macros [hiccups.core :refer [html]]))
 
-;;; 4 to 8, at least one numeric digit.
-(def ^:dynamic *password-re*
-  #"^(?=.*\d).{4,8}$")
-
-(def ^:dynamic *email-re*
-  #"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$")
-
 (defn validate-email [email]
   (destroy! (by-class "email"))
-  (if (not (re-matches *email-re* (value email)))
+  (if (not (re-matches (re-pattern (attr email :pattern)) (value email)))
     (do
-      (prepend! (by-id "loginForm") (html [:div.help.email "Wrong email"]))
+      (prepend! (by-id "loginForm") (html [:div.help.email (attr email :title)]))
       false)
     true))
 
 (defn validate-password [password]
   (destroy! (by-class "password"))
-  (if (not (re-matches *password-re* (value password)))
+  (if (not (re-matches (re-pattern (attr password :pattern)) (value password)))
     (do
-      (append! (by-id "loginForm") (html [:div.help.password "Wrong password"]))
+      (append! (by-id "loginForm") (html [:div.help.password (attr password :title)]))
       false)
     true))
 
